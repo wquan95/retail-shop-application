b2ab9324b54ea54232ecc81c68888b8c1c3f0390
makinoasia <makinoasia@users.noreply.github.com>
2026-06-05 07:41:53 +0800

    remove lockfile

--- terraform/vpc/version.tf
+++ terraform/vpc/version.tf
@@ -13,7 +13,6 @@ terraform{
         key             = "vpc/dev/terraform.tfstate"
         region          = "us-east-1"
         encrypt         = true
-        use_lockfile    = true
     } 
     
 }
